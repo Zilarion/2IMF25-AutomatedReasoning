928d2ed5c6e7b71e80ceb7ba6aa648bb3e0cbaf8
Rik Coenders <rik.coenders@gmail.com>
2016-12-09 15:53:15 +0100

    SMV version of exercise 4

---
+++ Assignment 1/exercise4a.smv.py
@@ -0,0 +1,32 @@
+from itertools import combinations
+
+def rng(*args):
+    def r(i, n):
+        return range(i, n+1)
+
+    return r(*args) if len(args) == 2 else r(1, args[0])
+
+def main(a0, e):
+    n = len(a0)
+
+    print('MODULE main')
+
+    print('VAR')
+    for i in rng(n):
+        print('a%d : -1000..1000;' % i)
+    print ('i : 2..%d;' % (n-1))
+
+    print('INIT')
+    print(' & '.join(['a%d = %d' % (i, a0[i-1]) for i in rng(n)]))
+
+    print('TRANS')
+    for i in rng(2, n-1):
+        print('%s next(i) = %d ? next(a%d) = a%d + a%d : a%d = a%d' % ((' ' if i==2 else '&'), i, i, i-1, i+1, i, i))
+    print('& next(a1) = a1')
+    print('& next(a%d) = a%d' % (n, n))
+
+    print('LTLSPEC G !(%s)' % ' & '.join(['a%d = a%d' % (i, j) for i, j in combinations(e, 2)]))
+
+a0 = rng(8)
+e = [3, 7]
+main(a0, e)
